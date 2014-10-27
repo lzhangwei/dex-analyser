bb1d0dede9d6afdd7779c2e1eb89c4b3b8531b4f
ZhangWei <lzhangwei@outlook.com>
2014-10-27 12:37:40 +0800

    add stringListRef field

--- src/StringTable.java
+++ src/StringTable.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class StringTable {
-    private List<String> stringList;
+    private List<Long> stringListRef = new ArrayList<Long>();
+    private List<String> stringList = new ArrayList<String>();
 
     public List<String> getStringList() {
         return stringList;
@@ -9,6 +11,22 @@ public class StringTable {
 
     public void setStringList(List<String> stringList) {
         this.stringList = stringList;
+    }
+
+    public List<Long> getStringListRef() {
+        return stringListRef;
+    }
+
+    public void setStringListRef(List<Long> stringListRef) {
+        this.stringListRef = stringListRef;
+    }
+
+    public void addStringList(String str) {
+        stringList.add(str);
+    }
+
+    public void addStringListRef(Long ref) {
+        stringListRef.add(ref);
     }
 
 }
