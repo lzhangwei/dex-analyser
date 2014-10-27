5de4514a1fed912634927a553a2d85124d8dcd6f
ZhangWei <lzhangwei@outlook.com>
2014-10-27 11:35:55 +0800

    add stringList

--- src/StringTable.java
+++ src/StringTable.java
@@ -1,2 +1,14 @@
+import java.util.List;
+
 public class StringTable {
+    private List<String> stringList;
+
+    public List<String> getStringList() {
+        return stringList;
+    }
+
+    public void setStringList(List<String> stringList) {
+        this.stringList = stringList;
+    }
+
 }
