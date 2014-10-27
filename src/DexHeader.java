4e313744e6f3acfc78bcea175bc113c249ad7908
ZhangWei <lzhangwei@outlook.com>
2014-10-27 16:28:27 +0800

    add readHeader function

--- src/DexHeader.java
+++ src/DexHeader.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 public class DexHeader {
     private RefAndSize stringTable;
     private RefAndSize typeTable;
@@ -53,4 +56,42 @@ public class DexHeader {
     public void setClassTable(RefAndSize classTable) {
         this.classTable = classTable;
     }
+
+    public void readHeader(InputStream inputStream) {
+        byte buffer4[] = new byte[4];
+        try {
+            inputStream.skip(56);
+            //string
+            stringTable = createRefAndSize(inputStream,buffer4);
+            //type
+            typeTable = createRefAndSize(inputStream,buffer4);
+            //prototype
+            protoTable = createRefAndSize(inputStream,buffer4);
+            //field
+            fieldTable = createRefAndSize(inputStream,buffer4);
+            //method
+            methodTable = createRefAndSize(inputStream,buffer4);
+            //class
+            classTable = createRefAndSize(inputStream,buffer4);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private RefAndSize createRefAndSize(InputStream inputStream, byte buffer4[]) {
+        RefAndSize result = new RefAndSize();
+        try {
+            inputStream.read(buffer4, 0, 4);
+            byte sizes[] = buffer4;
+            int size = Util.bytesToInt(sizes);
+            inputStream.read(buffer4, 0, 4);
+            byte offs[] = buffer4;
+            int off = Util.bytesToInt(offs);
+            result = new RefAndSize(off,size);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
 }
