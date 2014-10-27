609aea56bcf78bac6d3182bed14c659afaf625cc
ZhangWei <lzhangwei@outlook.com>
2014-10-27 23:19:45 +0800

    add fields

--- src/PrototypeRef.java
+++ src/PrototypeRef.java
@@ -1,2 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrototypeRef {
+    private int funDeclareStringRef;
+    private int returnTypeRef;
+    private int parameterTypeOff;
+    private int parameterSize;
+    private List<Integer> parameterTypesRef = new ArrayList<Integer>();
+
+    public int getFunDeclareStringRef() {
+        return funDeclareStringRef;
+    }
+
+    public void setFunDeclareStringRef(int funDeclareStringRef) {
+        this.funDeclareStringRef = funDeclareStringRef;
+    }
+
+    public int getReturnTypeRef() {
+        return returnTypeRef;
+    }
+
+    public void setReturnTypeRef(int returnTypeRef) {
+        this.returnTypeRef = returnTypeRef;
+    }
+
+    public int getParameterTypeOff() {
+        return parameterTypeOff;
+    }
+
+    public void setParameterTypeOff(int parameterTypeOff) {
+        this.parameterTypeOff = parameterTypeOff;
+    }
+
+    public int getParameterSize() {
+        return parameterSize;
+    }
+
+    public void setParameterSize(int parameterSize) {
+        this.parameterSize = parameterSize;
+    }
+
+    public List<Integer> getParameterTypesRef() {
+        return parameterTypesRef;
+    }
+
+    public void setParameterTypesRef(List<Integer> parameterTypesRef) {
+        this.parameterTypesRef = parameterTypesRef;
+    }
+
+    public PrototypeRef() {
+    }
 }
