89120046945c2b13d54567d7d2441fb2862f6b06
ZhangWei <lzhangwei@outlook.com>
2014-10-27 23:44:19 +0800

    add fields

--- src/PrototypeTable.java
+++ src/PrototypeTable.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PrototypeTable {
+    List<PrototypeRef> prototypeRefList = new ArrayList<PrototypeRef>();
+    List<Prototype> prototypeList = new ArrayList<Prototype>();
+
+    public List<PrototypeRef> getPrototypeRefList() {
+        return prototypeRefList;
+    }
+
+    public void setPrototypeRefList(List<PrototypeRef> prototypeRefList) {
+        this.prototypeRefList = prototypeRefList;
+    }
+
+    public List<Prototype> getPrototypeList() {
+        return prototypeList;
+    }
+
+    public void setPrototypeList(List<Prototype> prototypeList) {
+        this.prototypeList = prototypeList;
+    }
+
+    public PrototypeTable() {
+    }
+
+    public PrototypeTable(List<PrototypeRef> prototypeRefList, List<Prototype> prototypeList) {
+        this.prototypeRefList = prototypeRefList;
+        this.prototypeList = prototypeList;
+    }
 }
