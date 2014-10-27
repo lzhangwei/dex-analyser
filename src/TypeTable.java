fdca765f49a1c20051d6038302acdd9131f75811
ZhangWei <lzhangwei@outlook.com>
2014-10-27 20:50:53 +0800

    add fileds

--- src/TypeTable.java
+++ src/TypeTable.java
@@ -1,2 +1,28 @@
 public class TypeTable {
+    private String typeList;
+    private int typeStringRef;
+
+    public String getTypeList() {
+        return typeList;
+    }
+
+    public void setTypeList(String typeList) {
+        this.typeList = typeList;
+    }
+
+    public int getTypeStringRef() {
+        return typeStringRef;
+    }
+
+    public void setTypeStringRef(int typeStringRef) {
+        this.typeStringRef = typeStringRef;
+    }
+
+    public TypeTable() {
+    }
+
+    public TypeTable(String typeList, int typeStringRef) {
+        this.typeList = typeList;
+        this.typeStringRef = typeStringRef;
+    }
 }
