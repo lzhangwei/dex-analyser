75a947979d763a5578f6e686a2d3457ca6bd0929
ZhangWei <lzhangwei@outlook.com>
2014-10-27 12:02:49 +0800

    add off and size

--- src/RefAndSize.java
+++ src/RefAndSize.java
@@ -1,2 +1,28 @@
 public class RefAndSize {
+    private long off;
+    private int size;
+
+    public long getOff() {
+        return off;
+    }
+
+    public void setOff(long off) {
+        this.off = off;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public RefAndSize() {
+    }
+
+    public RefAndSize(long off, int size) {
+        this.off = off;
+        this.size = size;
+    }
 }
