d87b8eb65d2be46a4ccbbf0ac68f3bc5bcbb0afa
ZhangWei <lzhangwei@outlook.com>
2014-10-27 21:28:01 +0800

    add type

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
         DexHeader dexHeader = new DexHeader();
         FileInputStream inputStream = null;
+        byte buffer1[] = new byte[1];
+        byte buffer4[] = new byte[4];
 
         try {
             inputStream = new FileInputStream("resources/test.dex");
@@ -14,12 +16,13 @@ public class Main {
             inputStream.skip(8);//跳过DataSize&DataOff
 
             StringTable stringTable = new StringTable();
-
             stringTable.createStringListRef(inputStream, dexHeader.getStringTable().getSize());
+
+            TypeTable typeTable = new TypeTable();
+            typeTable.createTypeStringRef(inputStream, dexHeader.getTypeTable().getSize());
 
             System.out.println("Dex文件中字符串个数为：" + dexHeader.getStringTable().getSize());
             System.out.println("字符串依次是：");
-            byte buffer1[] = new byte[1];
             for (int i = 0; i < dexHeader.getStringTable().getSize(); i++) {
                 inputStream = new FileInputStream("resources/test.dex");
                 inputStream.skip(stringTable.getStringRef(i));
@@ -30,6 +33,14 @@ public class Main {
                 inputStream.read(buffer, 0, size);
                 stringTable.addStringList(new String(buffer));
                 System.out.println("     字符串"+(i+1)+":" + new String(buffer));
+            }
+
+            System.out.println("Dex文件中类型个数为：" + dexHeader.getTypeTable().getSize());
+            System.out.println("类型依次是：");
+            for (int i = 0; i < dexHeader.getTypeTable().getSize(); i++) {
+                int ref = typeTable.getTypeStringRef(i);
+                typeTable.addTypeList(stringTable.getStringList().get(ref-1));
+                System.out.println("     类型"+(i+1)+":" + stringTable.getStringList().get(ref-1));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
