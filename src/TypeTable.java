d59f73e723dab9894116b5535ff849de2f8f7bd6
ZhangWei <lzhangwei@outlook.com>
2014-10-27 20:55:46 +0800

    add addTypeList function

--- src/TypeTable.java
+++ src/TypeTable.java
@@ -28,4 +28,8 @@ public class TypeTable {
         this.typeList = typeList;
         this.typeStringRef = typeStringRef;
     }
+
+    public void addTypeList(String type) {
+        typeList.add(type);
+    }
 }
