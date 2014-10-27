da580116366cb49f3dab3b11d4d4ede6b8886a0b
ZhangWei <lzhangwei@outlook.com>
2014-10-27 20:54:32 +0800

    update fields

--- src/TypeTable.java
+++ src/TypeTable.java
@@ -1,27 +1,30 @@
-public class TypeTable {
-    private String typeList;
-    private int typeStringRef;
+import java.util.ArrayList;
+import java.util.List;
 
-    public String getTypeList() {
+public class TypeTable {
+    private List<String> typeList = new ArrayList<String>();
+    private List<Integer> typeStringRef = new ArrayList<Integer>();
+
+    public List<String> getTypeList() {
         return typeList;
     }
 
-    public void setTypeList(String typeList) {
+    public void setTypeList(List<String> typeList) {
         this.typeList = typeList;
     }
 
-    public int getTypeStringRef() {
+    public List<Integer> getTypeStringRef() {
         return typeStringRef;
     }
 
-    public void setTypeStringRef(int typeStringRef) {
+    public void setTypeStringRef(List<Integer> typeStringRef) {
         this.typeStringRef = typeStringRef;
     }
 
     public TypeTable() {
     }
 
-    public TypeTable(String typeList, int typeStringRef) {
+    public TypeTable(List<String> typeList, List<Integer> typeStringRef) {
         this.typeList = typeList;
         this.typeStringRef = typeStringRef;
     }
