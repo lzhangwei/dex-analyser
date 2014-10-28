50a1876bfb3ab2f35bcb6e14bf182253b35ce3fa
ZhangWei <lzhangwei@outlook.com>
2014-10-28 23:20:13 +0800

    add fields and functions

--- src/DexMethod.java
+++ src/DexMethod.java
@@ -1,2 +1,34 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 public class DexMethod {
+    private int methodRef;
+    private int accessFlag;
+    private int codeOff;
+
+    public int getMethodRef() {
+        return methodRef;
+    }
+
+    public int getAccessFlag() {
+        return accessFlag;
+    }
+
+    public int getCodeOff() {
+        return codeOff;
+    }
+
+    public void createDexMethod(InputStream inputStream) {
+        byte buffer4[] = new byte[4];
+        try {
+            inputStream.read(buffer4);
+            methodRef = Util.byte4ToInt(buffer4);
+            inputStream.read(buffer4);
+            accessFlag = Util.byte4ToInt(buffer4);
+            inputStream.read(buffer4);
+            codeOff = Util.byte4ToInt(buffer4);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
