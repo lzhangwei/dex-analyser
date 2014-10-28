81f8f1b57bcea66e0e366a90e868ed6c6e58956d
ZhangWei <lzhangwei@outlook.com>
2014-10-28 20:58:33 +0800

    add fields

--- src/FieldRef.java
+++ src/FieldRef.java
@@ -1,2 +1,38 @@
 public class FieldRef {
+    private int classTypeRef;
+    private int fieldTypeRef;
+    private int fieldNameRef;
+
+    public FieldRef() {
+    }
+
+    public FieldRef(int classTypeRef, int fieldTypeRef, int fieldNameRef) {
+        this.classTypeRef = classTypeRef;
+        this.fieldTypeRef = fieldTypeRef;
+        this.fieldNameRef = fieldNameRef;
+    }
+
+    public int getClassTypeRef() {
+        return classTypeRef;
+    }
+
+    public void setClassTypeRef(int classTypeRef) {
+        this.classTypeRef = classTypeRef;
+    }
+
+    public int getFieldTypeRef() {
+        return fieldTypeRef;
+    }
+
+    public void setFieldTypeRef(int fieldTypeRef) {
+        this.fieldTypeRef = fieldTypeRef;
+    }
+
+    public int getFieldNameRef() {
+        return fieldNameRef;
+    }
+
+    public void setFieldNameRef(int fieldNameRef) {
+        this.fieldNameRef = fieldNameRef;
+    }
 }
