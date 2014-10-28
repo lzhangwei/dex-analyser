859e7863538d57451263e057128ef383e9a4a44b
ZhangWei <lzhangwei@outlook.com>
2014-10-28 20:59:52 +0800

    add fields

--- src/Field.java
+++ src/Field.java
@@ -1,2 +1,38 @@
 public class Field {
+    private String classType;
+    private String fieldType;
+    private String fieldName;
+
+    public Field() {
+    }
+
+    public Field(String classType, String fieldName, String fieldType) {
+        this.classType = classType;
+        this.fieldName = fieldName;
+        this.fieldType = fieldType;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public void setClassType(String classType) {
+        this.classType = classType;
+    }
+
+    public String getFieldType() {
+        return fieldType;
+    }
+
+    public void setFieldType(String fieldType) {
+        this.fieldType = fieldType;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
 }
