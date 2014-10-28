751d31cb32781327ba1f871708ad50f25923ab82
ZhangWei <lzhangwei@outlook.com>
2014-10-28 21:02:33 +0800

    add fields

--- src/FieldTable.java
+++ src/FieldTable.java
@@ -4,5 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FieldTable {
-    
+    private List<Integer> fieldRefList = new ArrayList<Integer>();
+    private List<String> fieldList = new ArrayList<String>();
+
+    public FieldTable() {
+    }
+
+    public FieldTable(List<Integer> fieldRefList, List<String> fieldList) {
+        this.fieldRefList = fieldRefList;
+        this.fieldList = fieldList;
+    }
+
+    public List<Integer> getFieldRefList() {
+        return fieldRefList;
+    }
+
+    public void setFieldRefList(List<Integer> fieldRefList) {
+        this.fieldRefList = fieldRefList;
+    }
+
+    public List<String> getFieldList() {
+        return fieldList;
+    }
+
+    public void setFieldList(List<String> fieldList) {
+        this.fieldList = fieldList;
+    }
 }
