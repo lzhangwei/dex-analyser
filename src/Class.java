946eb87c2548cae227d3ec2ffd4e46645ad5415b
ZhangWei <lzhangwei@outlook.com>
2014-10-28 23:34:37 +0800

    update file

--- src/Class.java
+++ src/Class.java
@@ -1,2 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Class {
+    private String classType;
+    private int accessFlag;
+    private String superClassType;
+    private int interfaceSize;
+    private List<String> interfaceList = new ArrayList<String>();
+    private String sourceFileName;
+    //注解未实现
+    private ClassData classData;
+    //staticValues
+
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public int getAccessFlag() {
+        return accessFlag;
+    }
+
+    public String getSuperClassType() {
+        return superClassType;
+    }
+
+    public int getInterfaceSize() {
+        return interfaceSize;
+    }
+
+    public List<String> getInterfaceList() {
+        return interfaceList;
+    }
+
+    public String getSourceFileName() {
+        return sourceFileName;
+    }
+
+    public ClassData getClassData() {
+        return classData;
+    }
 }
