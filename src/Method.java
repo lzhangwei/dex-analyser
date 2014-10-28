42a35ccba1a45bb26ef0571e0446df0b63cc77ff
ZhangWei <lzhangwei@outlook.com>
2014-10-28 21:36:40 +0800

    add fields

--- src/Method.java
+++ src/Method.java
@@ -1,2 +1,20 @@
 public class Method {
+    private String classType;
+    private Prototype prototype;
+    private String methodName;
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public Prototype getPrototype() {
+        return prototype;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public Method() {
+    }
 }
