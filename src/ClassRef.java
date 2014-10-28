bd6304311171462f8b3fa501f4ddb83014eb16b8
ZhangWei <lzhangwei@outlook.com>
2014-10-28 22:54:51 +0800

    add createClassRef

--- src/ClassRef.java
+++ src/ClassRef.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ClassRef {
     private int classTypeRef;
     private int accessFlag;
@@ -38,6 +41,30 @@ public class ClassRef {
 
     public int getSourceFileNameRef() {
         return sourceFileNameRef;
+    }
+
+    public void createClassRef(InputStream inputStream) {
+        byte[] buffer4 = new byte[4];
+        try {
+            inputStream.read(buffer4);
+            classTypeRef = Util.byte4ToInt(buffer4);
+            inputStream.read(buffer4);
+            accessFlag = Util.byte4ToInt(buffer4);
+            inputStream.read(buffer4);
+            superClassTypeRef = Util.byte4ToInt(buffer4);
+            inputStream.read(buffer4);
+            interfacesOff = Util.byte4ToInt(buffer4);
+            inputStream.read(buffer4);
+            sourceFileNameRef = Util.byte4ToInt(buffer4);
+            inputStream.read(buffer4);
+            annotationsOff = Util.byte4ToInt(buffer4);
+            inputStream.read(buffer4);
+            classDataOff = Util.byte4ToInt(buffer4);
+            inputStream.read(buffer4);
+            staticValuesOff = Util.byte4ToInt(buffer4);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
