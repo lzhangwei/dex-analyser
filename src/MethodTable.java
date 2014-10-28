25dce3adc1386ec7f9fee516e3c4656c6d260b64
ZhangWei <lzhangwei@outlook.com>
2014-10-28 21:43:48 +0800

    add field

--- src/MethodTable.java
+++ src/MethodTable.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodTable {
+    private List<MethodRef> methodRefList = new ArrayList<MethodRef>();
+    private List<Method> methodList = new ArrayList<Method>();
+
+    public List<MethodRef> getMethodRefList() {
+        return methodRefList;
+    }
+
+    public List<Method> getMethodList() {
+        return methodList;
+    }
+
+    
 }
+
