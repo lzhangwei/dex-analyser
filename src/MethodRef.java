b9fdb85dc50983ebf770923ae237033f4739ec24
ZhangWei <lzhangwei@outlook.com>
2014-10-28 21:35:11 +0800

    add fields

--- src/MethodRef.java
+++ src/MethodRef.java
@@ -1,2 +1,20 @@
 public class MethodRef {
+    private int classTypeRef;
+    private int prototypeRef;
+    private int methodNameRef;
+
+    public int getClassTypeRef() {
+        return classTypeRef;
+    }
+
+    public int getPrototypeRef() {
+        return prototypeRef;
+    }
+
+    public int getMethodNameRef() {
+        return methodNameRef;
+    }
+
+    public MethodRef() {
+    }
 }
