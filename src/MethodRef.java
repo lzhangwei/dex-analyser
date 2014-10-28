2d10851fc825ee30ab8110d76e2c96bda951bc97
ZhangWei <lzhangwei@outlook.com>
2014-10-28 21:38:30 +0800

    add createMethodRef

--- src/MethodRef.java
+++ src/MethodRef.java
@@ -1,3 +1,6 @@
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MethodRef {
     private int classTypeRef;
     private int prototypeRef;
@@ -16,5 +19,20 @@ public class MethodRef {
     }
 
     public MethodRef() {
+    }
+
+    public void createMethodRef(InputStream inputStream) {
+        byte[] buffer2 = new byte[2];
+        byte[] buffer4 = new byte[4];
+        try {
+            inputStream.read(buffer2);
+            classTypeRef = Util.byte2ToInt(buffer2);
+            inputStream.read(buffer2);
+            prototypeRef = Util.byte2ToInt(buffer2);
+            inputStream.read(buffer4);
+            methodNameRef = Util.byte4ToInt(buffer4);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
