d674fbb4e4222e3d212222ab8163d021ec972852
ZhangWei <lzhangwei@outlook.com>
2014-10-29 08:26:20 +0800

    add fields

--- src/ClassTable.java
+++ src/ClassTable.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassTable {
+    private List<ClassRef> classRefList = new ArrayList<ClassRef>();
+    private List<Class> classList = new ArrayList<Class>();
+
+    public List<ClassRef> getClassRefList() {
+        return classRefList;
+    }
+
+    public List<Class> getClassList() {
+        return classList;
+    }
 }
