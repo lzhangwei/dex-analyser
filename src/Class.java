80b181946aa1e2471d396987b207c2e5c23cccf4
ZhangWei <lzhangwei@outlook.com>
2014-10-29 08:39:11 +0800

    add createClass function

--- src/Class.java
+++ src/Class.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,5 +41,28 @@ public class Class {
 
     public ClassData getClassData() {
         return classData;
+    }
+
+    public void createClass(ClassRef classRef, StringTable stringTable, TypeTable typeTable, InputStream inputStream) {
+        classType = typeTable.getTypeList().get(classRef.getClassTypeRef());
+        accessFlag = classRef.getAccessFlag();
+        superClassType = typeTable.getTypeList().get(classRef.getSuperClassTypeRef());
+
+        byte[] buffer4 = new byte[4];
+        byte[] buffer2 = new byte[2];
+        try {
+            if (classRef.getInterfacesOff() != 0) {
+                inputStream.read(buffer4);
+                interfaceSize = Util.byte4ToInt(buffer4);
+                for (int i = 0; i < interfaceSize; i++) {
+                    inputStream.read(buffer2);
+                    int typeIndex = Util.byte2ToInt(buffer2);
+                    interfaceList.add(typeTable.getTypeList().get(typeIndex));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sourceFileName = stringTable.getStringList().get(classRef.getSourceFileNameRef());
     }
 }
